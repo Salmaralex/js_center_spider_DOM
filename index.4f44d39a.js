97ae73ce8eca07c2f9452fe9e56d19a0bcb0c3b1
Nikita Kalina <kalinanikita001@gmail.com>
2024-08-12 19:49:34 +0300

    make build

---
+++ index.4f44d39a.js
@@ -0,0 +1,2 @@
+var e=document.getElementsByClassName("spider"),t=document.getElementsByClassName("wall"),l=window.getComputedStyle(t[0]),a=parseFloat(l.height),o=parseFloat(l.width),s=parseFloat(window.getComputedStyle(e[0]).height),d=parseFloat(window.getComputedStyle(e[0]).width);e[0].style.top=a/2-s/2+"px",e[0].style.left=o/2-d/2+"px";
+//# sourceMappingURL=index.4f44d39a.js.map
